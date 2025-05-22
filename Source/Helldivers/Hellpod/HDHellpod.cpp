d3d30aa653ebd8cae97081ade43e6b000a3ef238
HOME\rgr2718 <hyulgoo@users.noreply.github.com>
2025-05-23 00:23:38 +0900

    헬포드 강하 작업 중

--- Source/Helldivers/Hellpod/HDHellpod.cpp
+++ Source/Helldivers/Hellpod/HDHellpod.cpp
@@ -1,11 +1,20 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Hellpod/HDHellpod.h"
+#include "Define/HDDefine.h"
+#include "Components/BoxComponent.h"
 
 AHDHellpod::AHDHellpod()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	HellpodMesh = CreateDefaultSubobject<UStaticMeshComponent>("HellpodMesh");
+	SetRootComponent(HellpodMesh);
+
+	CollisionComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Collision"));
+	CollisionComponent->SetupAttachment(RootComponent);
+	CollisionComponent->SetCollisionProfileName(TEXT("BlockAll")); // TODO 프리셋 만들어서 설정
+	CollisionComponent->OnComponentHit.AddDynamic(this, &AHDHellpod::OnBoxeHit);
 }
 
 void AHDHellpod::BeginPlay()
@@ -24,4 +33,32 @@ void AHDHellpod::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void AHDHellpod::OnBoxeHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	NULL_CHECK(OtherActor);
+
+	IAbilitySystemInterface* OwnerAbilitySystemInterface = Cast<IAbilitySystemInterface>(OwnerActor);
+	NULL_CHECK(OwnerAbilitySystemInterface);
+
+	UAbilitySystemComponent* OwnerAbilitySystemComponent = OwnerAbilitySystemInterface->GetAbilitySystemComponent();
+	NULL_CHECK(OwnerAbilitySystemComponent);
+
+	ExcuteGameplayCue(OwnerAbilitySystemComponent, ImpactBlocklCueTag, Hit);
+}
+
+void AHDHellpod::SpawnCharacter()
+{
+	NULL_CHECK(CharacterClass);
+
+	UWorld* World = GetWorld();
+	VALID_CHECK(World);
+
+	AHDCharacterPlayer* CharacterPlayer = World->SpawnActor<AHDCharacterPlayer>(CharacterClass, GetActorTransform());
+	NULL_CHECK(CharacterPlayer);
+
+	AController* Controller = GetController();
+	NULL_CHECK(Controller);
+	Controller->Possess(Cast<APawn>(CharacterPlayer));
 }
--- Source/Helldivers/Hellpod/HDHellpod.h
+++ Source/Helldivers/Hellpod/HDHellpod.h
@@ -4,9 +4,12 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "GameplayTagContainer.h"
 #include "HDHellpod.generated.h"
 
 class UInputComponent;
+class AHDCharacterPlayer;
+class UBoxComponent;
 
 UCLASS()
 class HELLDIVERS_API AHDHellpod : public APawn
@@ -14,14 +17,31 @@ class HELLDIVERS_API AHDHellpod : public APawn
 	GENERATED_BODY()
 
 public:
-	explicit AHDHellpod();
+	explicit		AHDHellpod();
 
 protected:
-	virtual void BeginPlay() override;
+	virtual void	BeginPlay() override;
+	virtual void	Tick(float DeltaTime) override;
+	virtual void	SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+	
+	void			OnBoxeHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
-public:	
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+private:
+	void			SpawnCharacter();
 
-	virtual void OnHit
+private:
+    UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UStaticMeshComponent>	HellpodMesh;
+	
+    UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UBoxComponent>			CollisionComponent;
+	
+    UPROPERTY(EditDefaultsOnly)
+    float								FallSpeed;
+	
+    UPROPERTY(EditDefaultsOnly)
+    TSubclassOf<AHDCharacterPlayer>		CharacterClass;
+	
+    UPROPERTY(EditDefaultsOnly)
+	FGameplayCueTag						ImpactTag;
 };
